e0a3aac8c39f9954c48a8e8f721e8bcf9096d8a6
Nikhil <1837nik@gmail.com>
2026-03-06 14:17:39 +0530

    change colour blue to white of hero title

--- stacksquad_main/settings.py
+++ stacksquad_main/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(&hik$b@wbif+_p=mx70g=ls^s$&u3_(+7*=_mxwll2e&fhnd^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -121,10 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "stacksquad_main/static",
-    ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "stacksquad_main/static/",
+    ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
